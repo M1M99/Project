b82fef38ec85a342c619ef834dc04f427a76d961
ASUS <ASUS@DESKTOP-NB7MT4D>
2025-04-07 17:58:04 +0400

    micro changes

--- FinalAspReactAuction.Server/obj/Debug/net8.0/FinalAspReactAuction.Server.AssemblyInfo.cs
+++ FinalAspReactAuction.Server/obj/Debug/net8.0/FinalAspReactAuction.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalAspReactAuction.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4fa27ffe461e336081cfc2f991a9f032b9c326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099ac863c22f95f08a6b930ddf3453dc95599a2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalAspReactAuction.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalAspReactAuction.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
